49c4791b14709899ea07584d35adb933dd9764bc
AkaArtAnder <andrey@endaltsev.ru>
2023-04-27 18:38:31 +0300

    add base

---
+++ README.md
@@ -0,0 +1,1 @@
+# Environment developer with vscode inside docker
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int, char**) {
+    int a = 2;
+    std::cout << R"(Hello, world!)" << '\n';
+    std::cout << "Hello\n";
+}
